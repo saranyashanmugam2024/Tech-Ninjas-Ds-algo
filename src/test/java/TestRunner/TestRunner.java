70ff4568c5f8e587efbbfcaee0da6e8b4d37656b
saranyashanmugam2024 <157754905+saranyashanmugam2024@users.noreply.github.com>
2024-02-26 21:58:19 -0500

    Update TestRunner.java

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -6,16 +6,16 @@ package TestRunner;
 
 
 
-//import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;
 
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
-import io.cucumber.testng.*;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.testng.*;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
 
-//@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources/Feature",
 	
@@ -31,7 +31,7 @@ import io.cucumber.testng.CucumberOptions;
 		)
 
 
-public class TestRunner  
-extends AbstractTestNGCucumberTests{
+public class TestRunner  {
+//extends AbstractTestNGCucumberTests{
 
 }
